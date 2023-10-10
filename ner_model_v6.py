1cdd2790c2584a938d2d373f2b4d42beb54f2802
Katherine Phillips <75146063+kvphillips@users.noreply.github.com>
2023-10-10 11:56:53 -0700

    Update ner_model_v6.py

--- ner_model_v6.py
+++ ner_model_v6.py
@@ -1,8 +1,21 @@
+# Import the required programs to execute the commands of the spaCy code
+
 import spacy
 from spacy.training.example import Example
 
-nlp = spacy.load("ner_model_v5")  # Load your existing model
+# Load previously trained preliminary model
+# Minimal training occurred in ner_model_v5
+# A few catchphrases and labels related to health benefits and metabolites were used to construct the model correctly
+
+nlp = spacy.load("ner_model_v5")
+
+# Load the ner component
+
 ner = nlp.get_pipe("ner")
+
+# Included the list of labeled metabolites and health benefits within the list of code
+# This information was from .csv file that used additional python code to format in the following way
+# This data will be used to begin the first formal training of the model
 
 new_training_data = [
     {"text": "antioxidant", "entities": [[0, 11, "health benefit"]]},
@@ -38,15 +51,24 @@ new_training_data = [
     {"text": "atherosclerosis disease", "entities": [[0, 23, "health benefit"]]}
 ]
 
+# Convert the data to spaCy examples. Each training example makes a spaCy Doc object
+# It then makes a spaCy Example object from this Doc and the Example object is added to the new_examples list
+
 new_examples = []
 for data in new_training_data:
     doc = nlp.make_doc(data["text"])
     example = Example.from_dict(doc, data)
     new_examples.append(example)
 
-# Set the number of training iterations (e.g., 10)
+# Begin training
+
 nlp.begin_training()
+
+# Set the number of training iterations, this is the number of times it will run through the training data (e.g., 50)
+
 n_iter = 50
+
+# Update the model and drop the losses and print them to ensure that the model is not being overfitted
 
 for itn in range(n_iter):
     losses = {}
@@ -55,4 +77,5 @@ for itn in range(n_iter):
     print(losses)
 
 # Save the updated model
+
 nlp.to_disk("ner_model_v6")
