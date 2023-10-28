3bcda5a44cde7b1933f6433146bdedd820bc9d46
Katherine Phillips <75146063+kvphillips@users.noreply.github.com>
2023-10-27 21:31:41 -0700

    Update preprocessing_metabolite_data.py

--- preprocessing_metabolite_data.py
+++ preprocessing_metabolite_data.py
@@ -1,24 +1,33 @@
+#import corresponding libraries to run program
+
 import csv
 
-# Define the path to your CSV file
+# the path to your CSV file
+
 csv_file_path = "tomato_metabolite_v1.csv"
 
-# Define the path for the output text file
+# setup the path for the output text file that will be fed into the NER model
+
 output_file_path = "tomato_metabolite_v1.txt"
 
-# Initialize a list to store the data
+# setup list to store data
+
 data = []
 
-# Read the CSV file
+# read the CSV file
+
 with open(csv_file_path, newline='', encoding='utf-8') as csvfile:
     reader = csv.reader(csvfile)
     next(reader)  # Skip header rows
 
+# for loop to identify and make text and entity associations for the label metabolite    
+    
     for row in reader:
         if row:
             metabolite_name = row[0]
             data.append({"text": metabolite_name, "entities": [[0, len(metabolite_name), "metabolite"]]})
 
-# Write the data to the output text file
+# data is written to the output text file
+
 with open(output_file_path, 'w', encoding='utf-8') as output_file:
     output_file.write(str(data))
