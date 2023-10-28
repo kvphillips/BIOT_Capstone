f0df5116fabe6329d3b02c0c40cea9bf86031383
Katherine Phillips <75146063+kvphillips@users.noreply.github.com>
2023-10-27 21:40:25 -0700

    Update preprocessing_health_benefit_data.py

--- preprocessing_health_benefit_data.py
+++ preprocessing_health_benefit_data.py
@@ -1,24 +1,33 @@
+#import corresponding libraries to run program
+
 import csv
 
-# Define the path to your CSV file
+# the path to your CSV file
+
 csv_file_path = "tomato_health_v1.csv"
 
-# Define the path for the output text file
+# setup the path for the output text file that will be fed into the NER model
+
 output_file_path = "tomato_health_v1.txt"
 
-# Initialize a list to store the data
+# setup list to store the data
+
 data = []
 
-# Read the CSV file
+# read the CSV file
+
 with open(csv_file_path, newline='', encoding='utf-8') as csvfile:
     reader = csv.reader(csvfile)
     next(reader)  # Skip header rows
 
+#for loop to identify and make text and entity associations for the label health benefit
+    
     for row in reader:
         if row:
             health_benefit_name = row[0]
             data.append({"text": health_benefit_name, "entities": [[0, len(health_benefit_name), "health benefit"]]})
 
-# Write the data to the output text file
+# data is written to the output text file
+
 with open(output_file_path, 'w', encoding='utf-8') as output_file:
     output_file.write(str(data))
